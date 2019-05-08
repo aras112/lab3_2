82123b234ae8760f6736e0dc5d88b13260be0a27
Caroolinnee <karolinab341@gmail.com>
2019-05-08 16:27:06 +0200

    tested simple behavior

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -9,22 +9,31 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.iis.mto.staticmock.reader.NewsReader;
 
+
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
 public class NewsLoaderTest
     {
+
+    //behavior
     @Mock
     ConfigurationLoader configurationLoader;
     @Mock
     NewsReader newsReader;
+    @Mock
+    PublishableNews publishableNews;
 
-    Configuration configuration=new Configuration();
-    IncomingNews incomingNews= new IncomingNews();
+    //data struct
+
+    Configuration configuration = new Configuration();
+    IncomingNews incomingNews = new IncomingNews();
 
 
     @Before
@@ -35,10 +44,49 @@ public class NewsLoaderTest
         }
 
     @Test
-    public void loadNews()
+    public void simpleAddedOneASubscription()
         {
+        //given
+        incomingNews.add(new IncomingInfo("subsctiption", SubsciptionType.A));
 
+        //when
+        NewsLoader newsLoader=new NewsLoader();
+        newsLoader.loadNews();
 
+        //when
+        verify(publishableNews, times(1)).addForSubscription(any(String.class),any());
+
+        }
+
+    @Test
+    public void simpleAddedOneBSubscriptionAndLoadedTwoTimes()
+        {
+        //given
+        incomingNews.add(new IncomingInfo("subsctiption", SubsciptionType.B));
+
+        //when
+        NewsLoader newsLoader = new NewsLoader();
+        newsLoader.loadNews();
+        newsLoader.loadNews();
+
+        //then
+        verify(publishableNews, times(2)).addForSubscription(any(String.class), any());
+
+        }
+
+    @Test
+    public void simpleAddedOneCSubscriptionAndLoadedTwoTimes()
+        {
+        //given
+        incomingNews.add(new IncomingInfo("subsctiption", SubsciptionType.C));
+
+        //when
+        NewsLoader newsLoader = new NewsLoader();
+        newsLoader.loadNews();
+        newsLoader.loadNews();
+
+        //then
+        verify(publishableNews, times(2)).addForSubscription(any(String.class), any());
 
         }
 
@@ -47,6 +95,7 @@ public class NewsLoaderTest
         mockStatic(ConfigurationLoader.class);
         mockStatic(NewsReaderFactory.class);
         mockStatic(ConfigurationLoader.class);
+        mockStatic(PublishableNews.class);
         }
 
     private void setDefaultValue()
@@ -55,5 +104,6 @@ public class NewsLoaderTest
         when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
         when(NewsReaderFactory.getReader(any())).thenReturn(newsReader);
         when(newsReader.read()).thenReturn(incomingNews);
+        when(PublishableNews.create()).thenReturn(publishableNews);
         }
     }
